efc546a05b2f712aef08f42f4ebef4bc3924ee48
ComradeVanti <kingvanti650@gmail.com>
2022-04-10 18:36:32 +0200

    Add logic to replace title

--- src/base.js
+++ src/base.js
@@ -1,9 +1,20 @@
-function replaceLogo(){
-    const logoUrl =  chrome.runtime.getURL("logo.png");
+function replaceTitle() {
+    const emojis = [
+        ":)", "UwU", "OwO", "<3", ":P", ":3", ":*"
+    ]
+
+    const chosen = emojis[Math.floor(Math.random() * emojis.length)]
+
+    document.title = `eCampus ${chosen}`
+}
+
+function replaceLogo() {
+    const logoUrl = chrome.runtime.getURL("logo.png");
 
     Array.from(document.querySelectorAll("img"))
         .filter(it => it.src.includes("core_admin/logo"))
         .forEach(it => it.src = logoUrl)
 }
 
+replaceTitle()
 replaceLogo()
