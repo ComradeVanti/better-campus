5c9a9248c266ac0a7d45d613c4e2349a922a58f9
ComradeVanti <kingvanti650@gmail.com>
2022-05-13 11:22:12 +0200

    Add logic to replace page

--- src/base.js
+++ src/base.js
@@ -1,12 +1,49 @@
+const noMoving = {
+    extract: () => ({}),
+    inject: () => {
+    }
+}
+
+/**
+ * @param {string} html
+ * @return {Document}
+ */
+const makeDoc = (html) => {
+    const parser = new DOMParser();
+    return parser.parseFromString(html, "text/html");
+}
+
+/**
+ * @param {string} filePath
+ * @return {string}
+ */
 const getUrl = filePath =>
     chrome.runtime.getURL(`${filePath}`);
 
-const tryGetPagePath = () => {
+/**
+ * @return {string}
+ */
+const tryGetDirectoryPath = () => {
     const urlPath = window.location.pathname
     const directoryPath = urlPath.substring(0, urlPath.lastIndexOf("/") + 1)
-    return `/files/pages${directoryPath}page.html`
-};
+    return `/files/pages${directoryPath}`
+}
 
+/**
+ * @return {string}
+ */
+const tryGetPagePath = () =>
+    `${tryGetDirectoryPath()}page.html`;
+
+/**
+ * @return {string}
+ */
+const getMoveScriptPath = () =>
+    `${tryGetDirectoryPath()}move.js`;
+
+/**
+ * @return {Promise<html>}
+ */
 const tryGetPageHtml = () => {
     const path = tryGetPagePath()
     const url = getUrl(path)
@@ -15,22 +52,62 @@ const tryGetPageHtml = () => {
         .catch(_ => null);
 };
 
+/**
+ * @param {string} placeholder
+ * @return {string}
+ */
 const replacePlaceholder = placeholder =>
     getUrl(`files${placeholder.substring(5)}`)
 
+/**
+ * @param {html} rawHtml
+ * @return {html}
+ */
 const processHtml = rawHtml =>
     rawHtml.replaceAll(/\[ext\][^\"]*/gm, replacePlaceholder);
 
-const tryReplaceHtml = async () => {
+/**
+ * @return {Promise<MoveFunctions>}
+ */
+const tryGetMoveFunctions = () => {
+    const path = getMoveScriptPath()
+    const url = getUrl(path)
+    return import(url)
+        .then(m => ({extract: m.extract, inject: m.inject}))
+        .catch(_ => noMoving)
+}
+
+function runScripts() {
+
+    const run = (script) => {
+        const newScript = document.createElement("script")
+        newScript.src = script.src;
+        script.replaceWith(newScript)
+    }
+
+    Array.from(document.scripts).forEach(run)
+}
+
+async function tryReplaceHtml() {
     const rawHtml = await tryGetPageHtml()
     if (rawHtml != null) {
-        document.write(processHtml(rawHtml))
-        document.close()
+        const {extract, inject} = await tryGetMoveFunctions()
+        const data = extract(document)
+
+        const html = processHtml(rawHtml)
+        const newDoc = makeDoc(html)
+        inject(newDoc, data)
+
+        document.replaceChild(newDoc.documentElement, document.documentElement)
+
+        runScripts()
     }
-};
+}
 
-(async function init() {
+document.documentElement.style.display = "none"
+
+window.addEventListener("load", async function (event) {
+    event.stopImmediatePropagation();
     await tryReplaceHtml()
-}())
-
+}, true);
 
---
+++ src/types.js
@@ -0,0 +1,21 @@
+/**
+ * @callback ExtractData
+ * @param {Document} doc
+ * @return {Object}
+ */
+
+/**
+ * @callback InjectData
+ * @param {Document} doc
+ * @param {Object} data
+ */
+
+/**
+ * @typedef {Object} MoveFunctions
+ * @property {ExtractData} extract
+ * @property {InjectData} inject
+ */
+
+/**
+ * @typedef {string} html
+ */
