5b65004dc871efcdbc5713016b66fed208907da4
ComradeVanti <kingvanti650@gmail.com>
2022-05-22 16:52:48 +0200

    Add logic to exclude types files

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   pages: {
-    login: "src/pages/login/main.js"
-  }
+    login: "src/pages/login/main.js",
+  },
+  chainWebpack: (config) => {
+    config.plugin("copy").tap(([options]) => {
+      options.patterns[0].globOptions.ignore.push("**/types.js");
+      return [options];
+    });
+  },
 });
